06f30ea4ca4573880222677edf51d3740c10f821
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-01-19 14:31:16 +0100

    Importato google fonts

--- src/App.vue
+++ src/App.vue
@@ -124,6 +124,13 @@ export default {
 <style lang="scss">
 @import './style/general.scss';
 
+// Google Font
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400&display=swap');
+
+  * {
+    font-family: 'Open Sans', sans-serif;
+  }
+
   // Personalizzazione scrollbar
   ::-webkit-scrollbar{
     width:0.5em;
